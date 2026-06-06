70800703a1d7415109a7f50fc60ac11a6cfac50c
mmunozdominguez <m.munoz.dominguez@gmail.com>
2026-06-06 09:20:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // BC3 Manager Service Worker v8
-const CACHE = 'bc3-v12';
+const CACHE = 'bc3-v13';
 
 self.addEventListener('install', e => {
   // Activar inmediatamente sin esperar a que se cierren pestañas
